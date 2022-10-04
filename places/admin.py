3491210fed57a2d2a4c7ae5c0708c986acce1c60
HardRope <61661973+HardRope@users.noreply.github.com>
2022-10-04 23:47:42 +0300

    create Place and PlaceImage models

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Place, PlaceImage
 # Register your models here.
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(PlaceImage)
+class PlaceImageAdmin(admin.ModelAdmin):
+    pass
