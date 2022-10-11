4b0c9e6e68e21c015a42d82bc096a6287dcdd810
HardRope <61661973+HardRope@users.noreply.github.com>
2022-10-12 00:09:33 +0300

    add loading place info

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,6 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.show_main),
-    path('places/<int:place_id>/', views.place_view)
+    path('places/<int:place_id>/', views.place_view, name='places')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)\
               + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
