678c996a4956795f470208f7dc68f1358cec5d9c
HardRope <61661973+HardRope@users.noreply.github.com>
2022-10-11 22:57:50 +0300

    edit context view

--- mysite/views.py
+++ mysite/views.py
@@ -4,7 +4,7 @@ from places.models import Place, PlaceImage
 def show_main(request):
     locations = Place.objects.all()
     locations_format_geojson = []
-    for num, location in enumerate(locations):
+    for location in locations:
 
         formatted_location = {
             "type": "Feature",
@@ -14,10 +14,16 @@ def show_main(request):
             },
             "properties": {
                 "title": location.title,
-                "placeId": num,
+                "placeId": location.id,
                 "detailsUrl": ""
             }
         }
         locations_format_geojson.append(formatted_location)
-    context = {'geojson' : {"type": "FeatureCollection", 'features': locations_format_geojson}}
+
+    context = {'geojson' :
+        {
+        "type": "FeatureCollection",
+        'features': locations_format_geojson
+        }
+    }
     return render(request, 'index.html', context=context)
