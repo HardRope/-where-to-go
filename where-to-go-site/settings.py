9d0f8d8cc35353459bd59b6ad431d954e518c10f
HardRope <61661973+HardRope@users.noreply.github.com>
2022-10-25 00:21:24 +0300

    Revert "add staticfiles_dirs to .env"
    This reverts commit c7f77874608f4366a60f5a5fcc045cd4097f4bf1.

--- where-to-go-site/settings.py
+++ where-to-go-site/settings.py
@@ -127,9 +127,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = env('STATIC_URL')
-STATIC_ROOT = env('STATIC_ROOT')
+STATIC_ROOT = env('STATIC_ROOT', './assets')
 
-STATICFILES_DIRS = [env('STATICFILES_DIRS')]
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
